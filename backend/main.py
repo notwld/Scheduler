570d5461d67599ff7a274a98aa372a442faacd56
bajwa <johnwick0137@gmail.com>
2023-01-20 13:06:35 +0500

    Added Tables

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from algorithms import *
 from flask import Flask,request,jsonify
 
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
@@ -37,6 +39,42 @@ def priority():
 
     scheduler = PriorityScheduler(processes)
     return jsonify(scheduler.output())
+
+
+@app.route("/fcfs_table")
+def fcfs_table():
+    processes = [[1, 0, 3], [2, 2, 6], [3, 4, 4], [4, 6, 5]]
+    scheduler = FCFS(processes)
+    output = scheduler.output()
+    return render_template("fcfs_table.html", output=output)
+
+@app.route("/sjf_table")
+def sjf_table():
+    processes = [[1, 0, 3], [2, 2, 6], [3, 4, 4], [4, 6, 5]]
+    scheduler = SJF(processes)
+    output = scheduler.output()
+    return render_template("sjf_table.html", output=output)
+@app.route("/priority_table")
+def priority_table():
+    processes = [
+    Process("P1", 0, 5, 3), 
+    Process("P2", 2, 2, 1),  
+    Process("P3", 3, 3, 2),
+    Process("P4", 5, 1, 2) 
+    ]
+
+    scheduler = PriorityScheduler(processes)
+    output = scheduler.output()
+    return render_template("priority_table.html", output=output)
+
+@app.route("/rr_table")
+def rr_table():
+    processes = [['P1', 0, 8, 2], ['P2', 1, 4, 1], ['P3', 2, 9, 3], ['P4', 3, 5, 4]]
+    scheduler = RoundRobin(processes,4)
+    output = scheduler.output()
+    return render_template("rr_table.html", output=output)
+
+
 
 
 app.run(debug=True)
